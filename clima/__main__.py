29ea39b20e451ab8225b15d28ae6edf79be365aa
diego-garro <diego.garromolina@yahoo.com>
2021-05-09 21:57:19 -0600

    Add bar_plot method

--- clima/__main__.py
+++ clima/__main__.py
@@ -3,7 +3,7 @@ import pandas as pd
 import yaml
 
 from . import __version__
-from .graphics.bar_plots import barfrec_plot
+from .graphics.bar_plots import bar_plot, barfrec_plot
 from .graphics.contour_map import contour_map
 from .graphics.resume_table import generate_table
 from .graphics.time_series import single_time_series, time_series
@@ -120,6 +120,7 @@ def weather(station: str):
         "Month",
         "Day",
         "Hour",
+        "Weather_intensity",
         "Weather_description",
         "Weather_precipitation",
         "Weather_obscuration",
@@ -129,54 +130,79 @@ def weather(station: str):
     df = pd.read_csv(f"data/{station}/{station}_metars.csv", usecols=columns)
     df["Hour1_24"] = df["Hour"].replace(0, 24)
 
-    barfrec_plot(
-        df,
-        station,
-        "Weather_description",
-        weather="SH",
-        bp_label="Chubascos de lluvia (SHRA)",
-        save_as="shra",
-    )
-    barfrec_plot(
-        df,
-        station,
-        "Weather_description",
-        weather="TS",
-        bp_label="Tormenta eléctrica (TS ó TSRA)",
-        save_as="tsra",
-    )
-    barfrec_plot(
-        df,
-        station,
-        "Weather_precipitation",
-        weather="RA",
-        bp_label="Lluvia (RA)",
-        save_as="ra",
-    )
-    barfrec_plot(
-        df,
-        station,
-        "Weather_precipitation",
-        weather="DZ",
-        bp_label="Llovizna (DZ)",
-        save_as="dz",
-    )
-    barfrec_plot(
-        df,
-        station,
-        "Weather_obscuration",
-        weather="BR",
-        bp_label="Neblina (BR)",
-        save_as="br",
-    )
-    barfrec_plot(
-        df,
-        station,
-        "Weather_obscuration",
-        weather="FG",
-        bp_label="Niebla (FG)",
-        save_as="fg",
-    )
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_description",
+    #    weather="SH",
+    #    bp_label="Chubascos de lluvia (SHRA)",
+    #    save_as="shra",
+    #)
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_description",
+    #    weather="TS",
+    #    bp_label="Tormenta eléctrica (TS ó TSRA)",
+    #    save_as="tsra",
+    #)
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_precipitation",
+    #    weather="RA",
+    #    bp_label="Lluvia (RA)",
+    #    save_as="ra",
+    #)
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_precipitation",
+    #    weather="DZ",
+    #    bp_label="Llovizna (DZ)",
+    #    save_as="dz",
+    #)
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_obscuration",
+    #    weather="BR",
+    #    bp_label="Neblina (BR)",
+    #    save_as="br",
+    #)
+    #barfrec_plot(
+    #    df,
+    #    station,
+    #    "Weather_obscuration",
+    #    weather="FG",
+    #    bp_label="Niebla (FG)",
+    #    save_as="fg",
+    #)
+    bar_plot(df, station, "Weather_description", weather="TS", save_as="ts")
+    bar_plot(df, station, "Weather_precipitation", weather="RA", save_as="ra")
+    bar_plot(df, station, "Weather_obscuration", weather="BR", save_as="br")
+
+
+@cli.command()
+@click.argument("station", type=click.STRING)
+def cloud_height(station: str):
+    columns = [
+        "Year",
+        "Month",
+        "Day",
+        "Hour",
+        "Sky_layer1_height",
+        "Sky_layer2_height",
+        "Sky_layer3_height",
+        "Sky_layer4_height",
+    ]
+
+    station = station.lower()
+    df = pd.read_csv(f"data/{station}/{station}_metars.csv", usecols=columns)
+    df["Hour1_24"] = df["Hour"].replace(0, 24)
+
+    # barfrec_plot(df, station, "Sky_layer_height", bp_label="Techo de nubes", save_as="ceiling")
+    bar_plot(df, station, "Sky_layer_height")
 
 
 if __name__ == "__main__":
--- clima/graphics/bar_plots.py
+++ clima/graphics/bar_plots.py
@@ -1,9 +1,12 @@
+from decimal import ROUND_HALF_UP, Decimal
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from clima.graphics import DAYS_PER_MONTH, MONTHS, hours_range
+from clima.graphics import DAYS_PER_MONTH, MONTHS, hours_range, local_time_list
+from clima.logger_model import logger
 
 time_ranges = {19: 9, 13: 6, 24: 12}
 
@@ -19,7 +22,7 @@ def _handle_cavok(df: pd.DataFrame, hrange: int):
 
 def _handle_visibility(df: pd.DataFrame):
     unique_vals = df["Visibility"].unique()
-    ocurrences = np.count_nonzero(unique_vals < 5000.0)
+    ocurrences = np.count_nonzero(unique_vals <= 5000.0)
 
     if ocurrences > 0:
         return 1
@@ -37,16 +40,24 @@ def _handle_cloud(df: pd.DataFrame):
                 "Sky_layer4_height",
             ]
         ]
-        < 1500.0
+        <= 1500.0
     )
 
-    if ocurrences > 0:
+    if ocurrences > 5:
         return 1
 
     return 0
 
 
 def _handle_weather(df: pd.DataFrame, variable: str, weather: str):
+    df = df.query('Weather_intensity != "VC"')
+    
+    if weather == "RA":
+        df = df.query('Weather_description != "SH" and Weather_description != "TS"')
+
+    if weather == "FG":
+        df = df.query('Weather_intensity != "VC"')
+
     ocurrences = np.count_nonzero(df[variable] == weather)
 
     if ocurrences > 0:
@@ -68,12 +79,21 @@ def barfrec_plot(
     month_means = np.arange(12)
     hrange = len(hours_range(station))
 
+    logger.info(
+        f"Getting the monthly DataFrames for bar-frecuencies plot, variable: {variable}."
+    )
     for month in months:
         month_sum = 0
 
+        logger.info(
+            f"Getting the yearly DataFrames for bar-frecuencies plot, variable: {variable}."
+        )
         for year in years:
             month_df = df.query(f"Year == {year} and Month == {month}")
 
+            logger.info(
+                f"Getting the dayly DataFrames for bar-frecuencies plot, variable: {variable}, and calculating the means."
+            )
             for day in DAYS_PER_MONTH[month]:
                 day_df = month_df.query(f"Day == {day}")
 
@@ -86,7 +106,9 @@ def barfrec_plot(
                 else:
                     month_sum += _handle_weather(day_df, variable, weather)
 
-        month_means[month - 1] = month_sum / len(years)
+        month_means[month - 1] = Decimal(month_sum / len(years)).quantize(
+            Decimal("1."), ROUND_HALF_UP
+        )
     frecs = 100 * month_means / len(DAYS_PER_MONTH[month])
     print(month_means, frecs)
 
@@ -95,7 +117,7 @@ def barfrec_plot(
     bars = sns.barplot(
         x=months - 1, y=month_means, color="royalblue", ax=ax, label=bp_label
     )
-    ax.set(ylabel="No. ocurrencias mensuales")
+    ax.set(ylabel="No. de días con ocurrencia de fenomeno")
     ax.set_xlabel(None)
     ax_hanldes, ax_labels = ax.get_legend_handles_labels()
     ax2 = ax.twinx()
@@ -118,8 +140,65 @@ def barfrec_plot(
     )
     ax2_hanldes, ax2_labels = ax2.get_legend_handles_labels()
     all_handles = ax_hanldes + ax2_hanldes
+    ax.set_yticks(np.arange(0, 31, 5))
+    ax2.set_yticks(np.arange(0, 101, 10))
     ax.set_xticklabels([m[:3].upper() for m in MONTHS])
     sns.set()
     ax2.legend(handles=all_handles, loc="upper left", framealpha=0.9)
 
-    fig.savefig(f"template/Figures/graphs/barfrec_plot_{save_as}.png", format="png", dpi=600)
+    logger.info(f"Saving bar-frecuencies plot figure for variable {variable}.")
+    fig.savefig(
+        f"template/Figures/graphs/barfrec_plot_{save_as}.png", format="png", dpi=600
+    )
+
+
+def bar_plot(df: pd.DataFrame, station: str, variable: str, weather="", save_as=""):    
+    hours = hours_range(station)
+    means = np.arange(len(hours))
+    years = df["Year"].unique()
+
+    logger.info(
+        f"Getting the hourly DataFrames for bar plot, variable: {variable}, and calculating the means."
+    )
+    for hour in hours:
+        index = hours.index(hour)
+        hour_df = df.query(f"Hour1_24 == {hour}")
+
+        if variable == "Cavok":
+            mean = hour_df[variable].sum() / (12 * len(years))
+        elif variable == "Visibility":
+            vis_df = hour_df.query(f"{variable} <= 5000.0")
+            mean = vis_df["Visibility"].sum() / (12 * len(years))
+        elif variable in ["Weather_description", "Weather_obscuration"]:
+            mean = np.count_nonzero(hour_df[variable] == weather) / (12 * len(years))
+        elif variable == "Weather_precipitation":
+            if weather == "RA":
+                hour_df = hour_df.query(
+                    'Weather_description != "SH" and Weather_description != "TS"'
+                )
+            mean = np.count_nonzero(hour_df[variable] == weather) / (12 * len(years))
+        elif "Sky_layer" in variable:
+            cols = [
+                "Sky_layer1_height",
+                "Sky_layer2_height",
+                "Sky_layer3_height",
+                "Sky_layer4_height",
+            ]
+            mean = np.count_nonzero(hour_df[cols] <= 1500.0) / (12 * len(years))
+        else:
+            raise ValueError(f"Invalid variable {variable}.")
+
+        means[index] = Decimal(mean).quantize(Decimal("1."), ROUND_HALF_UP)
+
+    sns.set()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = sns.barplot(x=np.arange(0, len(hours)), y=means, color="royalblue", ax=ax)
+    ax.set_yticks(np.arange(0, 31, 5))
+    ax.set_xticklabels(local_time_list(hours))
+    ax.set_ylabel("No. de días con ocurrencia de fenómeno")
+
+    if station in ["mroc", "mrlb"]:
+        plt.xticks(rotation=90)
+
+    logger.info(f"Saving bar plot figure for variable {variable}" + ": {weather}." if weather != "" else ".")
+    fig.savefig(f"template/Figures/graphs/bar_plot_{save_as}.png", format="png", dpi=600)
